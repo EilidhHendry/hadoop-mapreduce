772ca6d1782ca944c64132cbb9e74a95ef5ffe43
eilidhhendry <ehendry91@gmail.com>
2014-12-02 19:41:00 +0000

    bug fix

--- task4mapper_part2.py
+++ task4mapper_part2.py
@@ -10,5 +10,5 @@ for line in sys.stdin:
     post_type = re.findall(r'PostTypeId="(.*?)"', line)[0]
     if post_type == '2':
         owner_id = re.findall(r'OwnerUserId="(.*?)"', line)[0]
-        post_id = re.findall(r'PostId="(.*?)"', line)[0]
+        post_id = re.findall(r' Id="(.*?)"', line)[0]
         print "%s\t%s" % (owner_id, post_id)
