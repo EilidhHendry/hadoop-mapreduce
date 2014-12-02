c81344820f1996b72a7109d3791884f68a548733
eilidhhendry <ehendry91@gmail.com>
2014-12-02 18:45:00 +0000

    sorting numerically

--- 4_part1.sh
+++ 4_part1.sh
@@ -10,7 +10,7 @@ NUMR=1
 
 hadoop dfs -rmr $OUT
 
-RUN="hadoop jar /opt/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-streaming.jar -D mapred.reduce.tasks=$NUMR -input $IN -output $OUT -mapper $MAP -file $MAP -reducer $RED -file $RED"
+RUN="hadoop jar /opt/hadoop/hadoop-0.20.2/contrib/streaming/hadoop-0.20.2-streaming.jar -D mapred.output.key.comparator.class=org.apache.hadoop.mapred.lib.KeyFieldBasedComparator -D mapred.text.key.comparator.options=-nr -D mapred.reduce.tasks=$NUMR -input $IN -output $OUT -mapper $MAP -file $MAP -reducer $RED -file $RED"
 $RUN
 rm -r $OUTFILE
 hadoop dfs -copyToLocal $OUT $OUTFILE
