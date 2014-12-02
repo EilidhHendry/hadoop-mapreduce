d9e698e516962ae26034b9867cd4cfbc24a7ecd4
eilidhhendry <ehendry91@gmail.com>
2014-12-02 18:41:17 +0000

    bug fix in q4p1

--- task4mapper_part1.py
+++ task4mapper_part1.py
@@ -7,8 +7,8 @@ import re
 
 for line in sys.stdin:
     line = line.strip()
-    post_type = re.findall(r'PostTypeId="(.*?)"', line)
+    post_type = re.findall(r'PostTypeId="(.*?)"', line)[0]
     if post_type == '1':
-        post_id = re.findall(r' Id="(.*?)"', line)
-        view_count = re.findall(r'ViewCount="(.*?)"', line)
+        post_id = re.findall(r' Id="(.*?)"', line)[0]
+        view_count = re.findall(r'ViewCount="(.*?)"', line)[0]
         print "%s\t%s" % (view_count, post_id)
